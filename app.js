cc7157e9319a25eecf8ec98592bb78c451e3466a
Miguel Branco <miguel.branco@easypay.pt>
2022-05-19 17:38:15 +0100

    [CR-1435] Demo page to show Checkout and SDK

---
+++ app.js
@@ -0,0 +1,107 @@
+const express = require('express')
+const fs = require('fs')
+const https = require('https')
+const path = require('path')
+require('dotenv').config()
+
+const app = express()
+const port = 3000
+
+// TODO: remove after inclusion from CDN
+app.use('/files', express.static(path.join(__dirname, 'files')))
+
+app.get('/checkoutmanifest', createCheckoutSession)
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.listen(port, () => {
+  console.log(`Checkout page running on http://localhost:${port}`)
+})
+
+/**
+ * Uses the Account ID and API Key to send an authenticated request from this server
+ * to easypay's APIs, creating a new Checkout Session.
+ * Receives a Checkout Manifest and returns it to the client, which will use it
+ * to initialize the Checkout SDK in the client side.
+ */
+function createCheckoutSession(req, res) {
+  const accountId = process.env.TEST_ACCOUNT_ID
+  const apiKey = process.env.TEST_API_KEY
+  const tomorrow = new Date()
+  tomorrow.setDate(tomorrow.getDate() + 1)
+  const host = 'api.test.easypay.pt'
+  const payload = JSON.stringify({
+    type: ['single'],
+    payment: {
+      methods: [
+        'cc',
+        'mb',
+        'mbw',
+        'dd',
+        'vi',
+        'uf',
+        'sc',
+      ],
+      "type": "sale",
+      "capture": {
+        "transaction_key": "string",
+        "descriptive": "Descriptive Example"
+      },
+      "currency": "EUR",
+      "expiration_time": tomorrow.toISOString().slice(0, 16).replace('T', ' ') // e.g. 2022-01-02 18:25
+    },
+    order: {
+      value: 2,
+      key: 'order-key',
+      items: [
+        {
+          description: 'Item in cart',
+          quantity: 1,
+          key: 'product-1',
+          value: 1
+        },
+        {
+          description: 'Item 2 in cart',
+          quantity: 1,
+          key: 'product-2',
+          value: 1
+        },
+      ]
+    },
+    config: null,
+    customer: {
+      "name": "Customer Example",
+      "email": "customer@example.com",
+      "phone": "911234567",
+      "phone_indicative": "+351",
+      "fiscal_number": "PT123456789",
+      "key": "Key Example",
+      "language": "PT"
+    }
+  })
+  const options = {
+    hostname: host,
+    path: '/2.0/checkout',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': payload.length,
+      'AccountId': accountId,
+      'ApiKey': apiKey
+    }
+  }
+  var httpreq = https.request(options, function (response) {
+    let manifest = ''
+    response.on('data', function (chunk) {
+      manifest += chunk
+    });
+    response.on('end', function() {
+      console.log('received: ', manifest)
+      res.send(manifest);
+    })
+  });
+  httpreq.write(payload)
+  httpreq.end()
+}
---
+++ files/bundle.js
@@ -0,0 +1,145 @@
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+    typeof define === 'function' && define.amd ? define(['exports'], factory) :
+    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.easypayCheckout = {}));
+})(this, (function (exports) { 'use strict';
+
+    var __assign = (window && window.__assign) || function () {
+        __assign = Object.assign || function(t) {
+            for (var s, i = 1, n = arguments.length; i < n; i++) {
+                s = arguments[i];
+                for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                    t[p] = s[p];
+            }
+            return t;
+        };
+        return __assign.apply(this, arguments);
+    };
+    /**
+     * The default values for all the options.
+     */
+    var defaultOptions = {
+        id: 'easypay-checkout',
+        onMessage: function () { },
+        testing: false,
+    };
+    /**
+     * Encapsulates a Checkout instance created by startCheckout.
+     * Stores useful parameters and allows cleanup when unmount() is called.
+     */
+    var CheckoutInstance = /** @class */ (function () {
+        /**
+         * The class constructor. Sets up the iframe contents and event listener.
+         */
+        function CheckoutInstance(manifest, givenOptions) {
+            var _a;
+            this.manifest = manifest;
+            this.givenOptions = givenOptions;
+            this.originUrl = CheckoutInstance.PROD_URL;
+            this.messageHandler = null;
+            this.options = __assign(__assign({}, defaultOptions), givenOptions);
+            if (!this.validateParameters(manifest, this.options)) {
+                return;
+            }
+            if (this.options.testing) {
+                this.originUrl = CheckoutInstance.TEST_URL;
+            }
+            this.messageHandler = this.handleMessage.bind(this);
+            window.addEventListener('message', this.messageHandler);
+            var iframe = document.createElement('iframe');
+            iframe.setAttribute('src', "".concat(this.originUrl, "?manifest=").concat(this.encodeManifest(manifest)));
+            // Using the attributes below in order to provide defaults without overriding CSS styles
+            iframe.setAttribute('width', '400');
+            iframe.setAttribute('height', '700');
+            iframe.setAttribute('frameborder', '0');
+            (_a = document.getElementById(this.options.id)) === null || _a === void 0 ? void 0 : _a.appendChild(iframe);
+        }
+        /**
+         * Validates the necessary parameters for Checkout initialization and gives helpful messages for integrators.
+         */
+        CheckoutInstance.prototype.validateParameters = function (manifest, options) {
+            if (!manifest || typeof manifest.id !== 'string' || manifest.id === '' || typeof manifest.session !== 'string' || manifest.session === '') {
+                console.error("".concat(CheckoutInstance.LOGTAG, " Please provide a valid Checkout Manifest when calling startCheckout."));
+                return false;
+            }
+            if (typeof options.id !== 'string' || options.id === '') {
+                console.error("".concat(CheckoutInstance.LOGTAG, " The HTML element id must be a non-empty string."));
+                return false;
+            }
+            var el = document.getElementById(options.id);
+            if (!el) {
+                console.error("".concat(CheckoutInstance.LOGTAG, " Could not find element ").concat(options.id, "."));
+                return false;
+            }
+            if (typeof options.onMessage !== 'function') {
+                console.error("".concat(CheckoutInstance.LOGTAG, " The onMessage callback must be a function."));
+                return false;
+            }
+            if (typeof options.testing !== 'boolean') {
+                console.error("".concat(CheckoutInstance.LOGTAG, " The testing option must be true or false."));
+                return false;
+            }
+            return true;
+        };
+        /**
+         * Encodes a Manifest for iframe consumption. Supports both Node and browser environments.
+         */
+        CheckoutInstance.prototype.encodeManifest = function (manifest) {
+            if (typeof Buffer !== 'undefined') {
+                return Buffer.from(JSON.stringify(manifest), 'binary').toString('base64');
+            }
+            else {
+                return window.btoa(JSON.stringify(manifest));
+            }
+        };
+        /**
+         * Handles messages sent from the Checkout iframe. If the origin and contents are as expected,
+         * pass them on to the message handler that was configured in startCheckout.
+         *
+         * If the Checkout becomes completed, automatically removes the event listener.
+         */
+        CheckoutInstance.prototype.handleMessage = function (e) {
+            if (e.origin === this.originUrl && e.data.type === 'ep-checkout') {
+                this.options.onMessage(e.data.status);
+                if (e.data.status === 'complete') {
+                    if (this.messageHandler) {
+                        window.removeEventListener('message', this.messageHandler);
+                    }
+                }
+            }
+        };
+        /**
+         * Used to cleanup Checkout by removing the DOM contents and event listener.
+         */
+        CheckoutInstance.prototype.unmount = function () {
+            var _a;
+            if (this.messageHandler) {
+                window.removeEventListener('message', this.messageHandler);
+            }
+            var children = Array.from(((_a = document.getElementById(this.options.id)) === null || _a === void 0 ? void 0 : _a.children) || []);
+            children.map(function (child) {
+                child.remove();
+            });
+        };
+        CheckoutInstance.PROD_URL = 'https://checkout-serverless.quality-utility.aws.easypay.pt';
+        CheckoutInstance.TEST_URL = 'https://checkout-serverless.sandbox.easypay.pt';
+        CheckoutInstance.LOGTAG = '[easypay Checkout SDK]';
+        return CheckoutInstance;
+    }());
+    /**
+     * Used to configure and populate the Checkout form.
+     *
+     * Returns an instance of the CheckoutInstance class, which can be used to manage
+     * the running Checkout (in particular, to unmount it).
+     */
+    function startCheckout(manifest, options) {
+        if (options === void 0) { options = {}; }
+        return new CheckoutInstance(manifest, options);
+    }
+
+    exports.CheckoutInstance = CheckoutInstance;
+    exports.startCheckout = startCheckout;
+
+    Object.defineProperty(exports, '__esModule', { value: true });
+
+}));
