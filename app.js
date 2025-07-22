4638500d2f406b8d2f0ac3805662c4ad081ad692
António Figueiredo <antonio.figueiredo@easypay.pt>
2025-07-22 12:55:02 +0100

    [CHEC-20] Support other ports in checkout demo

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path')
 require('dotenv').config()
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // TODO: remove after inclusion from CDN
 app.use('/images', express.static(path.join(__dirname, 'images')))
